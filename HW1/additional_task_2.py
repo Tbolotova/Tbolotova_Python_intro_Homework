039c2bbee23942115aec8a16dc997501fac820ed
Tbolotova <secret048@yandex.ru>
2023-09-01 15:03:55 +0300

    added additional_task_2 to HW1

--- HW1/additional_task_2.py
+++ HW1/additional_task_2.py
@@ -1,5 +1,6 @@
-# Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-# теперь надо проверить ее практически
+# Напишите программу для проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z
+# для всех значений предикат.
+# Теперь надо проверить ее практически
 # в цикле 100 раз прогоняем
 # каждый раз генерируем случайное количество предикат от 3 до 15
 # и конечно со случайным булевым значением
@@ -7,7 +8,29 @@
 # юзаем библиотеки random и time
 # предикаты НЕ ЗАДАЕМ как целое число!
 
-def check_logical_expression(predicates):
-    is_correct = False
-    print(is_correct)
+import random
+import time
 
+
+def check_logical_expression(predicates, result_list):
+    left = not all(predicates)
+    right = [not el for el in predicates]
+    is_correct = not all(predicates) == [not el for el in predicates]
+    result_list.append(is_correct)
+    print(f"Expression for {predicates} is {is_correct}")
+
+
+def generate_predicates():
+    predicate_num = random.randint(3, 15)
+    predicate_list = []
+    for k in range(1, predicate_num):
+        predicate_list.append(random.choice([True, False]))
+    return predicate_list
+
+
+start_time = time.time()
+check_list = []
+for i in range(100):
+    check_logical_expression(generate_predicates(), check_list)
+print(f"The result of the 100 checks for all predicates is: {all(check_list)}")
+print(f"Total time of execution is {time.time() - start_time}")
